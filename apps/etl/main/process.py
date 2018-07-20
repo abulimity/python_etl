5327fb2cdf551d92f2d2b7dceca0b3ae597781b6
abulimity <18082895096@163.com>
2018-07-20 12:35:23 +0800

    准备链接数据库，开发传输部分

--- apps/etl/main/process.py
+++ apps/etl/main/process.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import json
 
 
@@ -6,14 +7,15 @@ class TaskProcesser():
 
     def __init__(self):
         self.user_id = {
-            'mk':'mk/mk@wtbzdb1',
-            'sjgl':'sjgl/Ju8THs_sj@SJGL_NEW'
+            '7':'mk/mk@wtbzdb1',
+            '8':'sjgl/Ju8THs_sj@SJGL_NEW'
         }
 
     def take_task(self,task_argsr):
         # taskArgsr['task_status'] = 'recived'
         csv_path = task_argsr['source_file_path']
         csv_name = csv_path.replace('\\','/').split('/')[-1]
+
         # 创建任务目录
         os.chdir( '/'.join(csv_path.replace('\\','/').split('/')[:-1]))
         if csv_name.split('.')[0] in os.listdir():
@@ -21,23 +23,44 @@ class TaskProcesser():
         else:
             os.mkdir(csv_name.split('.')[0])
             os.chdir(csv_name.split('.')[0])
+
+        shutil.copy(task_argsr['source_file_path'],'.')
+
         # ctl
         ctl_name = csv_name.split('.')[0] + '.ctl'
+        with open(csv_name,'r',encoding='gbk') as f:
+            title_line = f.readline()
+        title = [l.replace('\n','') for l in title_line.split(',')]
+        truncate = 'truncate' if task_argsr['truncate']=='1' else ''
+        _column_str = ''
+        for t in title:
+            _column_str += t + '  ' + 'char(200)' +',\n'
+        column_str = _column_str[:-2]
+        ctl_content = '''
+LOAD DATA 
+INFILE '{0}'
+INTO TABLE {1}
+{2}
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
+TRAILING NULLCOLS
+(
+{3}
+)
+        '''.format(csv_name,task_argsr['target_table'],truncate,column_str)
+        with open(ctl_name,'w+',encoding='utf-8') as ctl_f:
+            ctl_f.write(ctl_content)
 
         # bat
         bat_name = csv_name.split('.')[0] + '.bat'
-        bat_f = open(bat_name,'w+',encoding='utf-8')
-        if task_argsr['target_contain']=='7':
-            userid = self.user_id['mk']
+        userid = self.user_id[task_argsr['target_container']]
         bat_content = '''
         sqlldr userid={0} control={1} log={2} bad={3}
-        '''
-        bat_f.write(bat_content)
+        '''.format(userid,ctl_name,csv_name.split('.')[0] + '.log',csv_name.split('.')[0] + '.bad')
+
+        with open(bat_name, 'w+', encoding='utf-8') as bat_f:
+            bat_f.write(bat_content)
 
         return  os.getcwd()
-
-    def create_csv(self):
-        pass
 
     def reviewCSV(self,filePath):
         csv_file = open(filePath,'r',encoding='gbk')
@@ -63,13 +86,13 @@ class TaskProcesser():
 if __name__ == '__main__':
     task_argsr = {'target_container': '7',
                   'task_status': 'recived',
-                  'target_table': 'ddd',
+                  'target_table': 'python_etl_test',
                   'source_table': '', 'target_sql': '',
-                  'source_file_path': 'E:\\project\\python_etl\\media\\upload\\etl\\杨哲_20180718170220.csv',
+                  'source_file_path': 'E:\\project\\python_etl\\media\\upload\\etl\\abulimity_20180718170220.csv',
                   'user_name': '杨哲',
                   'truncate': '0',
                   'source_sql': '',
                   'source_container': '1'}
 
     test = TaskProcesser()
-    print(test.taketask(task_argsr))
+    print(test.take_task(task_argsr))
